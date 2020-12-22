7eb2ef84afb937098bed48f5e45df081c315a0b7
Romich <70742227+Romell-rom@users.noreply.github.com>
2020-12-22 13:26:42 +0300

    Create VictorianChair.java

---
+++ laba21-22/Chairs/VictorianChair.java
@@ -0,0 +1,12 @@
+package laba21-22;
+
+public class VictorianChair implements Chair {
+    public VictorianChair() {
+        System.out.println("Викторианский стул у вас)");
+    }
+
+    @Override
+    public void sit() {
+        System.out.println("Вы сели на викторианский стул");
+    }
+}
