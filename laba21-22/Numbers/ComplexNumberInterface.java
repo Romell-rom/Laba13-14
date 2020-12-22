cc77d84fdc997551ec6298ec3ec12075b8d8f686
Romich <70742227+Romell-rom@users.noreply.github.com>
2020-12-22 13:31:32 +0300

    Create ComplexNumberInterface.java

---
+++ laba21-22/Numbers/ComplexNumberInterface.java
@@ -0,0 +1,24 @@
+package laba21-22;
+
+public interface ComplexNumberInterface{
+    double getRe();
+    double getIm();
+    double getModule();
+    String toString();
+    String sign();
+    static ComplexNumber divide() {
+        return null;
+    }
+
+    static ComplexNumber subtract() {
+        return null;
+    }
+
+    static ComplexNumber multiply() {
+        return null;
+    }
+
+    static ComplexNumber sum() {
+        return null;
+    }
+}
