8980a83f27724175ec4ce8730b26eb71f8bddcc3
Romich <70742227+Romell-rom@users.noreply.github.com>
2020-12-22 13:35:48 +0300

    Create RedactorMain.java

---
+++ laba21-22/Redach/RedactorMain.java
@@ -0,0 +1,9 @@
+package laba21-22;
+
+import java.io.IOException;
+
+public class RedactorMain {
+    public static void main(String[] args) throws IOException {
+        Karkas karkas=new Karkas();
+    }
+}
