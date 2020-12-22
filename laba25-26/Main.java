6f231638183cf16c64cedac92d9a0f3e2cf11aa9
Romich <70742227+Romell-rom@users.noreply.github.com>
2020-12-22 13:39:00 +0300

    Create Main.java

---
+++ laba25-26/Main.java
@@ -0,0 +1,9 @@
+package laba25-26;
+
+import java.io.EOFException;
+
+public class Main25_26 {
+    public static void main(String[] args) throws EOFException {
+        Parser parser = new Parser();
+    }
+}
